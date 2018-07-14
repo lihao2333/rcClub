c0fe82417aad35acee0e0a5c0ee1ad0ded234b67
lihao <lihao3016@gmail.com>
2018-07-14 10:41:27 +0800

    delete sh

--- hub.sh
+++
@@ -1,8 +0,0 @@
-files=`ls base*`
-if [ $# -ne 1 ]
-	then 
-		echo pls input current branch name ! 白痴
-	else
-		git checkout master
-		git checkout $1 $files
-fi
--- migrate.sh
+++
@@ -1,8 +0,0 @@
-files=`ls base* hub.sh`
-if [ $# -ne 1 ]
-	then 
-		echo stubid you are! whats the branch name?
-	else
-		git checkout $1
-		git checkout master $files
-fi
